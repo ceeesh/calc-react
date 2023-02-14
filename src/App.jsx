42e9cf3a5db3c7e861046e65cc7bbb57006a3353
Cee-Jay Malacas <ceejaymalacas17@gmail.com>
2023-02-14 15:01:06 +0800

    first design added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
     for (let i = 1; i < 10; i++) {
       digits.push(
-        <button key={i} className="max-w-[80px] max-h-[50px]">{i}</button>
+        <button key={i} className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center h-full w-full bg-1btnsbg m-auto rounded">{i}</button>
       )
     }
     return digits
@@ -27,10 +27,30 @@ function App() {
         <div className="screen bg-1screen h-[20%] w-full mt-4 rounded">
 
         </div>
-        <div className="buttons mt-4 bg-1btns h-[80%] w-full rounded grid">
-          <div className="grid grid-cols-3 grid-rows-3 col-span-4 row-span-4">
-          {createDigits()}
+        <div className="buttons mt-4 bg-1btns h-[80%] w-full rounded grid p-[12px] grid-cols-4 grid-rows-5 text-1tcolor text-[36px]">
+
+          <div className="numbers grid grid-cols-3 grid-rows-3 col-start-1 col-end-4 row-start-1 row-end-4 p-[5px] gap-[10px]">
+            {createDigits()}
           </div>
+
+          <div className="symbols1 flex flex-col col-start-4 col-end-5 row-start-1 row-end-5 p-[5px] gap-[15px] mt-1">
+            <button className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center w-full h-full text-[22px] rounded bg-1delbtn text-white">DEL</button>
+            <button className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center w-full h-full bg-1btnsbg rounded">+</button>
+            <button className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center w-full h-full bg-1btnsbg rounded">-</button>
+            <button className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center w-full h-full bg-1btnsbg rounded">x</button>
+          </div>
+
+          <div className="ml-1 symbols2 flex col-start-1 col-end-4 row-start-4 row-end-5 gap-[19px] p-[5px]">
+            <button className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center w-full h-full bg-1btnsbg rounded">.</button>
+            <button className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center w-full h-full bg-1btnsbg rounded">0</button>
+            <button className="max-w-[80px] max-h-[50px] text-center flex justify-center items-center w-full h-full bg-1btnsbg rounded">/</button>
+          </div>
+          
+          <div className="functions flex col-start-1 col-end-5 row-start-5 row-end-6 gap-[10px] 5-[5px] justify-between items-center mx-2">
+            <button className="max-w-[180px] w-full max-h-[50px] rounded m-auto text-center bg-1reset flex justify-center items-center text-white">RESET</button>
+            <button className="max-w-[180px] w-full max-h-[50px] rounded m-auto text-center bg-1eql flex justify-center items-center text-white">=</button>
+          </div>
+
         </div>
       </div>
     </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,12 @@ module.exports = {
       colors: {
         '1blue': 'hsl(222, 26%, 31%)',
         '1screen': '#182034',
-        '1btns': '#232C43'
+        '1btns': '#232C43',
+        '1tcolor': '#3B414E',
+        '1btnsbg': 'hsl(30, 25%, 89%)',
+        '1delbtn': 'hsl(225, 21%, 49%)',
+        '1reset': 'hsl(225, 21%, 49%)',
+        '1eql': 'hsl(6, 63%, 50%)',
       },
     },
   },
